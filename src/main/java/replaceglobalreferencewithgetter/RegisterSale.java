2e5846427325b63f526f93fc087d8b86f7e0b436
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-12-16 09:53:54 +0100

    Replace Global Reference With Getter implementation

--- src/main/java/replaceglobalreferencewithgetter/RegisterSale.java
+++ src/main/java/replaceglobalreferencewithgetter/RegisterSale.java
@@ -7,8 +7,12 @@ public class RegisterSale {
     private final List<Item> items = new ArrayList<>();
 
     public void addItem(Barcode code) {
-        Item newItem = Inventory.getInventory().itemForBarcode(code);
+        Item newItem = getInventory().itemForBarcode(code);
         items.add(newItem);
+    }
+
+    protected Inventory getInventory() {
+        return Inventory.getInventory();
     }
 
     public List<Item> getItems() {
--- src/test/java/replaceglobalreferencewithgetter/RegisterSaleShould.java
+++ src/test/java/replaceglobalreferencewithgetter/RegisterSaleShould.java
@@ -5,17 +5,24 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 class RegisterSaleShould {
 
     @Test
     void add_item() {
-//        Inventory.getInstance();
-        RegisterSale registerSale = new RegisterSale();
+        RegisterSale registerSale = new TestableRegisterSale();
         registerSale.addItem(new Barcode());
 
         List<Item> actual = registerSale.getItems();
 
         assertThat(actual).hasSize(1);
+    }
+
+    private class TestableRegisterSale extends RegisterSale {
+        @Override
+        protected Inventory getInventory() {
+            return mock(Inventory.class);
+        }
     }
 }
